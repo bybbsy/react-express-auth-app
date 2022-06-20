10d15995febc08ad1ab6ba251eecbe4b27f3295a
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-20 10:17:34 +0300

    Smth

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,46 +1,16 @@
-import { createUseStyles } from "react-jss";
 import { Link } from "react-router-dom";
-
-const useNavbarStyles = createUseStyles({
-    header: {
-        display: 'flex',
-        height: '45px',
-        alignItems: 'center',
-        backgroundColor: 'teal',
-
-    },
-    header__inner: {
-        margin: '0 20px',
-        width: '100%',
-    },
-    'header-navbar': {
-        display: 'flex', 
-    },
-    'header-navbar__links': {
-        display: 'flex',
-        margin: '0 0 0 auto'
-    },
-    'header-navbar__item': {
-        margin: '0 5px',
-    },
-    'header-navbar__link': {
-        color: 'white'
-    }
-})
-
-function Navbar() {
-    const navbarStyles = useNavbarStyles();
-
+ 
+function navbar() {
     return (
-        <header className={navbarStyles.header}>
-            <div className={navbarStyles.header__inner}>
-                <nav className={navbarStyles['header-navbar']}>
-                    <ul className={navbarStyles["header-navbar__links"]}>
-                        <li className={navbarStyles["header-navbar__item"]}>
-                            <Link to="/sign-in" className={navbarStyles["header-navbar__link"]}>Sign in</Link>
+        <header className="header">
+            <div className="header__inner">
+                <nav className="header-navbar">
+                    <ul className="header-navbar__links">
+                        <li>
+                            <Link to="/sign-in" className="header-navbar__link">Sign in</Link>
                         </li>
                         <li>
-                            <Link to="/sign-up" className={navbarStyles["header-navbar__link"]}>Sign up</Link>
+                            <Link to="/sign-up" className="header-navbar__link">Sign up</Link>
                         </li>
                     </ul>
                 </nav>
@@ -49,4 +19,4 @@ function Navbar() {
     )
 }
 
-export default Navbar;
+export default navbar;
