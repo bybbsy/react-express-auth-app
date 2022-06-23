39ac8f7f0c5394b20e5b1dd61cbe93a8e5e337e1
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-23 11:29:03 +0300

    Added tokens maniplations, removed mock data

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,125 +1,71 @@
 const { Router } = require('express')
 const UserModel = require('../models/user')
 const TokenModel = require('../models/token')
+const { createTokens, saveRefreshToken, validateRefreshToken, validateAccessToken } = require('../service/token-service')
 
 const jwt = require('jsonwebtoken')
 const AuthServiceError = require('../models/error')
 
 const router = Router()
 
-const posts = [
-    {
-        username: 'Jeff',
-        title: "Jeff's title"
-    },
-    {
-        username: 'Jimmy',
-        title: "Jimmi's post"
-    },
-    {
-        username: 'Kyle',
-        title: "Kyle's story"
-    }
-]
-
-
-router.post('/login', (req, res) => {
-    const username = req.body.username;
-    const user = { name: username };
-
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '15s' });
-    res.json({
-        user,
-        accessToken
-    })
-})
-
-function validateAccessToken(token) {
-    try {
-        const userData = jwt.verify(token, process.env.ACCESS_TOKEN);
-        return userData;
-    } catch (e) {
-        return null;
-    }
-}
-
-function validateRefreshToken(token) {
-    try {
-        const userData = jwt.verify(token, process.env.REFRESH_TOKEN);
-        return userData;
-    } catch (e) {
-        return null;
-    }
-}
-
 router.post('/sign-up', async (req, res) => {
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const candidate = await UserModel.findOne({email})
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
     
-    if(candidate) {
-        throw AuthServiceError.InvalidRequest("User already exists!")
-    }
-
-    const user = await UserModel.create({ email, password })
-
-    const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN, { expiresIn: '15s'})
-    const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_TOKEN, { expiresIn: '30s'})
-
-    const tokenData = await TokenModel.findOne({ user: user._id })
+        const candidate = await UserModel.findOne({email})
+        
+        if(candidate) {
+            throw AuthServiceError.InvalidRequest('User already exists!')
+        }
     
-    let token;
-    if(tokenData) {
-        tokenData.refreshToken = refreshToken;
-        await tokenData.save();
-    } else {
-        token = await TokenModel.create({ user: user._id, refreshToken })
+        const user = await UserModel.create({ email, password })
+    
+        const tokens = createTokens(user.toJSON())
+
+        const tokenData = await TokenModel.findOne({ user: user._id })
+        
+        await saveRefreshToken(tokenData, user, tokens.refreshToken);
+    
+        res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000 });
+    
+        res.send({ user, tokens })
+
+    } catch (e) {
+        return res.status(e.status).json({msg: e.message});   
     }
-
-    res.cookie('refreshToken', refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000 });
-
-    res.send({ user, accessToken, refreshToken })
 })
 
 router.post('/sign-in', async (req, res) => {
-    const { email, password } = req.body;
- 
-    const user = await UserModel.findOne({email});
- 
-    if(!user) {
-        throw AuthServiceError.Unauthorized();
-    }
+    try {
+        const { email, password } = req.body;
 
-    const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN, { expiresIn: '15s'})
-    const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_TOKEN, { expiresIn: '30s'})
-
-    const tokenData = await TokenModel.findOne({ user: user._id })
+        console.log(email)
+        const user = await UserModel.findOne({email});
     
-    let token;
+        if(!user) {
+            throw AuthServiceError.Unauthorized();
+        }
 
-    if(tokenData) {
-        tokenData.refreshToken = refreshToken;
-        await tokenData.save();
-    } else {
-        token = await TokenModel.create({ user: user._id, refreshToken })
+        const tokens = createTokens(user.toJSON())
+
+        const tokenData = await TokenModel.findOne({ user: user._id })
+
+        await saveRefreshToken(tokenData, user, tokens.refreshToken)
+
+        res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false})
+        
+        res.send({user, tokens})
+
+    } catch (e) {
+        return res.status(e.status).json({msg: e.message});   
     }
-
-    res.cookie('refreshToken', refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false});
-    
-    const tokens = {
-        accessToken,
-        refreshToken
-    }
-
-    res.send({user, tokens})
 })
 
 router.post('/sign-out', async (req, res) => {
     const { refreshToken } = req.cookies
     const tokenData = await TokenModel.deleteOne({ refreshToken })
-
+    
     res.clearCookie('refreshToken');
 
     res.send({
@@ -130,71 +76,31 @@ router.post('/sign-out', async (req, res) => {
 
 router.post('/refresh', async (req, res) => {
     try {
-
         const rt = req.cookies.refreshToken;
- 
+        
         const userData = validateRefreshToken(rt)
+
         const tokenInDB = await TokenModel.findOne({ refreshToken: rt })
-    
+
         console.log(!userData || !tokenInDB)
         if(!userData || !tokenInDB) {
             throw AuthServiceError.Unauthorized()
         }
 
         const user = await UserModel.findById(userData._id)
-        const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN, { expiresIn: '15s'})
-        const refreshToken = jwt.sign(user.toJSON(), process.env.REFRESH_TOKEN, { expiresIn: '30s'})
+
+        const tokens = createTokens(user.toJSON())
     
         const tokenData = await TokenModel.findOne({ user: user._id })
-        
-        let token;
-    
-        if(tokenData) {
-            tokenData.refreshToken = refreshToken;
-            await tokenData.save();
-        } else {
-            token = await TokenModel.create({ user: user._id, refreshToken })
-        }
-    
-        res.cookie('refreshToken', refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000 });
-    
-        const tokens = {
-            accessToken,
-            refreshToken
-        }
 
-        res.send({user, tokens});
+        await saveRefreshToken(tokenData, user, tokens.refreshToken)
+    
+        res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false})
+
+        res.send({user, tokens})
     } catch (e) {
-        console.log(e)
         return res.status(e.status).json({msg: e.message});   
     }
 })
-
-
-router.get('/posts', authenticateToken, (req, res) => {
-    console.log('Request:', req.user)
-     
-    const userPosts = posts.filter(post => post.username === req.user.name)
-    res.send({
-        data: userPosts
-    })
-    
-})
-
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    console.log('Auth-Header', token)
-
-    if(token == null) return res.sendStatus(401);
-    
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
-        if(err) return res.sendStatus(403); 
-        req.user = user
-        next()
-    })
- 
-}
-
 
 module.exports = router;
