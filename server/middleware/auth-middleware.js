6cd45c579a077ad0bad9ba6cdce07fecf22c6e60
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-23 11:24:17 +0300

    Added auth middleware for validating access token

---
+++ server/middleware/auth-middleware.js
@@ -0,0 +1,23 @@
+const AuthServiceError = require('../models/error')
+const TokenService = require('../service/token-service')
+
+module.exports = function(req, res, next) {
+    try {
+        const accessToken = req.headers['authorization'].split(' ')[1]
+         
+        if(!accessToken) {
+            return next(AuthServiceError.Unauthorized())
+        }
+
+        const accessTokenIsValid = TokenService.validateAccessToken(accessToken)
+         
+        if(!accessTokenIsValid) {
+            return next(AuthServiceError.Unauthorized())
+        } 
+
+        req.user = accessTokenIsValid
+        next()
+    } catch(e) {
+        next(e)
+    }
+}
