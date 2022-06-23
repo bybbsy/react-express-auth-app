ae3c3bfbf686a486839533aca76277fa2358abf5
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-23 11:30:21 +0300

    Added route for cards

---
+++ server/routes/cardList.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const CardModel = require('../models/card')
+const authMiddleware = require('../middleware/auth-middleware')
+
+const router = Router()
+
+router.get('/cards', authMiddleware, async (req, res) => {
+    try { 
+        const cards = await CardModel.find()
+     
+        res.send(cards)
+    } catch (e) {
+        console.log('e')
+        res.send({ status: e.status, message: e.message })
+    }
+})
+
+module.exports = router
