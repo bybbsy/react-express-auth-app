aa948094e573973c1de5e49d51bc3f7a48764ee2
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-23 11:31:01 +0300

    Added token service

---
+++ server/service/token-service.js
@@ -0,0 +1,41 @@
+
+const TokenModel = require('../models/token')
+const jwt = require('jsonwebtoken')
+
+exports.createTokens = function(user) {
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '15s' })
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN, { expiresIn: '60s' })
+
+    return {
+        accessToken,
+        refreshToken
+    }
+}
+
+exports.saveRefreshToken = async function(tokenData, user, refreshToken) {
+    if (tokenData) {
+        tokenData.refreshToken = refreshToken;
+        return tokenData.save();
+    }
+
+    const token = await TokenModel.create({ user: user._id, refreshToken })
+    return token;
+}
+
+exports.validateAccessToken = function(token) {
+    try {
+        const userData = jwt.verify(token, process.env.ACCESS_TOKEN);
+        return userData;
+    } catch (e) {
+        return null;
+    }
+}
+
+exports.validateRefreshToken = function(token) {
+    try {
+        const userData = jwt.verify(token, process.env.REFRESH_TOKEN);
+        return userData;
+    } catch (e) {
+        return null;
+    }
+}
