b17f01eead0466ce8f0f9973773891700140f8ac
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-27 13:16:08 +0300

    Addjusted swagger config

--- server/swagger/config.js
+++ server/swagger/config.js
@@ -3,12 +3,23 @@ const swaggerJsDoc = require('swagger-jsdoc')
 const options = {
     definition: {
         openapi: '3.0.1',
-        info: 'JWT-authentication server',
-        version: '1.0.0'
+        info: {
+            title: 'JWT Server',
+            description: 'Desc',
+        },
+        version: '1.0.0',
+        servers: [
+            {
+                url: 'http://localhost:3000/api',
+                description: 'A server for testing JWT auth API'
+            }
+        ],
     },
     apis: [
         './routes/*.js'
     ]
 }
 
-module.exports = swaggerJsDoc(options)
+const config = swaggerJsDoc(options)
+console.log(config)
+module.exports = config
