f2601710cb7bfb0cf7596efe105bc807fda55869
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-29 11:19:43 +0300

    Removed swagger annotations

--- server/routes/cardList.js
+++ server/routes/cardList.js
@@ -3,32 +3,13 @@ const CardModel = require('../models/card')
 const authMiddleware = require('../middleware/auth-middleware')
 
 const router = Router()
- 
-/**
- * @swagger
- * /cards:
- *  get:
- *   description: Returns list of cards for authenticated users
- *   summary: Returns list of cards for authenticated users 
- *   tags:
- *    - Cards API
- *   responses:
- *      200:
- *       description: successful operation
- *      422:
- *       description: Invalid operation (Email or password are invalid or user already exists)
- *   security:
- *      - bearerAuth: []
- */  
+
 router.get('/cards', authMiddleware, async (req, res) => {
-    console.log('access')
     try { 
-        console.log('access')
         const cards = await CardModel.find()
      
         res.send(cards)
     } catch (e) {
-        console.log('e')
         res.send({ status: e.status, message: e.message })
     }
 })
