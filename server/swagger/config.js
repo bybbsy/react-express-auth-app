2677647ef2daa0efafeb487affa3feddd1f0e6ba
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-29 11:19:55 +0300

    Added swagger docs for routes

--- server/swagger/config.js
+++ server/swagger/config.js
@@ -86,11 +86,369 @@ const options = {
                 }
             }
         },
+        paths: {
+            '/sign-in' : {
+                post: {
+                    description: 'Accepts user credentials to authenticate and returns access and refresh tokens',
+                    summary: 'Authenticates existing user',
+                    tags: [
+                        'Auth API'
+                    ],
+                    requestBody: {
+                        description: 'Accepts user credentials to sign-in user and returns access and refresh tokens, registers accessToken in db',
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'object',
+                                    properties: {
+                                        email: {
+                                            type: 'string',
+                                            required: true
+                                        },
+                                        password: {
+                                            type: 'string',
+                                            required: true
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'Successful sign-in',
+                            headers: {
+                                'Set-Cookie': {
+                                    description: 'Contains JWT refresh token (refreshToken=JWT)',
+                                    type: 'string'
+                                }
+                            },
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            accessToken: {
+                                                type: 'string'
+                                            },
+                                            refreshToken: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        422: {
+                            description: "Invalid email or password or user doesn't exist",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        500: {
+                            description: "Some internal server error",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            '/sign-up' : {
+                post: {
+                    description: 'Accepts user credentials to register user and returns access and refresh tokens, registers user in db',
+                    summary: 'Registers a new user account',
+                    tags: [
+                        'Auth API'
+                    ],
+                    requestBody: {
+                        description: 'Accepts user credentials to sign-up user and returns access and refresh tokens, registers user and accessToken in db',
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'object',
+                                    properties: {
+                                        email: {
+                                            type: 'string',
+                                            required: true
+                                        },
+                                        password: {
+                                            type: 'string',
+                                            required: true
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'Successful sign-up',
+                            headers: {
+                                'Set-Cookie': {
+                                    description: 'Contains JWT refresh token (refreshToken=JWT)',
+                                    type: 'string'
+                                }
+                            },
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            user: {
+                                                type: 'object',
+                                                properties: {
+                                                    email: {
+                                                        type: 'string',
+                                                    },
+                                                    password: {
+                                                        type: 'string'
+                                                    }
+                                                }
+                                            },
+                                            tokens: {
+                                                type: 'object',
+                                                properties: {
+                                                    accessToken: {
+                                                        type: 'string'
+                                                    },
+                                                    refreshToken: {
+                                                        type: 'string'
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        422: {
+                            description: "Invalid email or password or user doesn't exist",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        500: {
+                            description: "Some internal server error",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            '/sign-out': {
+                post: {
+                    description: 'Signs out user',
+                    summary: 'Signs out user',
+                    tags: [
+                        'Auth API'
+                    ],
+                    responses: {
+                        200: {
+                            description: 'Successful log out'
+                        },
+                        500: {
+                            description: "Some internal server error",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            '/refresh' : {
+                post: {
+                    description: 'Refreshes user tokens',
+                    summary: 'Refreshes user tokens',
+                    tags: [
+                        'Auth API'
+                    ],
+                    responses: {
+                        200: {
+                            description: 'Successful refresh',
+                            headers: {
+                                'Set-Cookie': {
+                                    description: 'Contains JWT refresh token (refreshToken=JWT)',
+                                    type: 'string'
+                                }
+                            },
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            user: {
+                                                type: 'object',
+                                                properties: {
+                                                    email: {
+                                                        type: 'string',
+                                                    },
+                                                    password: {
+                                                        type: 'string'
+                                                    }
+                                                }
+                                            },
+                                            tokens: {
+                                                type: 'object',
+                                                properties: {
+                                                    accessToken: {
+                                                        type: 'string'
+                                                    },
+                                                    refreshToken: {
+                                                        type: 'string'
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        422: {
+                            description: "Invalid token or provided token wasn't stored in database (it was expired or doesn't belong to this user)",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        500: {
+                            description: "Some internal server error",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            '/cards': {
+                get: {
+                    description: 'Returns list of cards for authenticated users',
+                    summary: 'Returns list of cards for authenticated users ',
+                    tags: [
+                        'Cards API'
+                    ],
+                    responses: {
+                        200: {
+                            description: "Successful operation",
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'array',
+                                        items: {
+                                            $ref: '#/components/schemas/Card'
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        422: { 
+                            description: 'If access or refresh tokens are invalid',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        500: {
+                            description: 'Some internal server error',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'object',
+                                        properties: {
+                                            msg: {
+                                                type: 'string'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    security: [
+                        {
+                            bearerAuth: []
+                        }
+                    ]
+                }
+            }
+        }
     },
     apis: [
         './routes/*.js'
-    ],
-
+    ]
 }
 
 const config = swaggerJsDoc(options)
