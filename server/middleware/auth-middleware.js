dfdb14e22fdf8050908209f07922aae7b0a98edc
Kirill Pulnev <kirill_pulnev@senla.eu>
2022-06-29 11:20:26 +0300

    Added authHeader validation

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -1,23 +1,28 @@
 const AuthServiceError = require('../models/error')
 const TokenService = require('../service/token-service')
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
     try {
-         
-        const accessToken = req.headers['authorization'].split(' ')[1]
-        if(!accessToken) {
-            return next(AuthServiceError.Unauthorized())
+        const authHeader = req.headers['authorization']
+
+        if (authHeader) {
+            const accessToken = authHeader.split(' ')[1]
+
+            if (!accessToken) {
+                return next(AuthServiceError.Unauthorized())
+            }
+
+            const accessTokenIsValid = TokenService.validateAccessToken(accessToken)
+
+            if (!accessTokenIsValid) {
+                return next(AuthServiceError.Unauthorized())
+            }
+
+            req.user = accessTokenIsValid
+            next()
         }
-
-        const accessTokenIsValid = TokenService.validateAccessToken(accessToken)
-         
-        if(!accessTokenIsValid) {
-            return next(AuthServiceError.Unauthorized())
-        } 
-
-        req.user = accessTokenIsValid
-        next()
-    } catch(e) {
+        next(AuthServiceError.Unauthorized())
+    } catch (e) {
         next(e)
     }
 }
